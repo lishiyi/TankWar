dfcb2d03e155da4c3f3f68f7fa1ef9f212d9a057
lishiyi <lishiyiiii@gmail.com>
2015-11-20 17:22:01 -0500

    Basic
    * Window
    * Backgroud
    * Move

---
+++ src/shiyi/TankClient.java
@@ -0,0 +1,89 @@
+package shiyi;
+
+import java.awt.*;
+import java.awt.event.*;
+
+public class TankClient extends Frame{
+	
+	public static final int GAME_WIDTH = 800;
+	public static final int GAME_HEIGHT = 600;
+	
+	private int x = 50, y = 50;
+	
+	Image offScreenImage = null;
+	
+	
+	@Override
+	public void paint(Graphics g) {
+		
+		Color c = g.getColor();
+		g.setColor(Color.red);
+		g.fillOval(x, y, 30, 30);
+		g.setColor(c);
+		
+		y += 5;
+	}
+	
+	public void update(Graphics g){
+		
+		if(offScreenImage == null){
+			offScreenImage = this.createImage(GAME_WIDTH, GAME_HEIGHT);
+			
+		}
+		Graphics goffScreen = offScreenImage.getGraphics();
+		Color c = goffScreen.getColor();
+		goffScreen.setColor(Color.gray);
+		goffScreen.fillRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
+		goffScreen.setColor(c);
+		paint(goffScreen);
+		g.drawImage(offScreenImage, 0, 0, null);
+	}
+	
+	public void lauchFrame(){
+		
+		this.setLocation(400, 300);
+		this.setSize(GAME_WIDTH, GAME_HEIGHT);
+		this.setTitle("Tank War");
+		this.addWindowListener(new WindowAdapter(){
+			@Override
+			public void windowClosing(WindowEvent e) {
+				
+				System.exit(0);
+			}
+		});
+		this.setResizable(false);
+		this.setBackground(Color.gray);
+		
+		this.addKeyListener(new KeyMonitor());
+		
+		setVisible(true);
+		
+		new Thread(new PaintThread()).start();
+	}
+	public static void main(String[] args) {
+		
+		TankClient tc = new TankClient();
+		tc.lauchFrame();
+	}
+	
+	private class PaintThread implements Runnable{
+		
+		public void run(){
+			while(true){
+				repaint();
+				try {
+					Thread.sleep(100);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+	
+	private class KeyMonitor extends KeyAdapter{
+		
+		public void keyPressed(KeyEvent e){
+			System.out.println("OK!!!!");
+		}
+	}
+}
